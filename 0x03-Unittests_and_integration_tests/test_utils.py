d66925c30341dcffdf1d9bba1a2e44b3b5ad704b
dahunsi-dami <gbemigadare@gmail.com>
2024-08-26 13:04:54 +0000

    Test for KeyError raise w/ wrong key input.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,6 +19,15 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b"),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, error):
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), f"'{error}'")
+
 
 if __name__ == '__main__':
     unittest.main()
