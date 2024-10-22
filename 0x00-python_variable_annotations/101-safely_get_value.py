b3005ebaa5efd6c84a3fc3910a422109e5f8787d
dahunsi-dami <gbemigadare@gmail.com>
2024-10-23 00:22:05 +0100

    Add type annots to safely_get_value func as specced.

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Type annotated function safely_get_value."""
+
+from typing import Mapping, Any, Union, TypeVar
+
+T = TypeVar('T')
+
+
+# The types of the elements of the input are not known
+def safely_get_value(dct: Mapping[Any, Any], key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+    """
+    Returns value associated w/ key in the dictionary-
+    -or the default if the key doesn't exist.
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
