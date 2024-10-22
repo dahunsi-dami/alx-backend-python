9dac4001388929b68e5db7aa5eb4b9e6f3a69f1f
dahunsi-dami <gbemigadare@gmail.com>
2024-10-22 23:41:18 +0100

    Make type annot func to return tuple as specced.

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Type-annotated function to_kv."""
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Returns tuple w/ str k & square of v as a float."""
+    return (k, float(v ** 2))
