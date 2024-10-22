046d25bdeeb77674a1f9b5d6e5ae0d150ad7bccb
dahunsi-dami <gbemigadare@gmail.com>
2024-10-23 00:41:58 +0100

    Break prototype line up to fix pycodestyle length requirements.

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -7,7 +7,11 @@ T = TypeVar('T')
 
 
 # The types of the elements of the input are not known
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+def safely_get_value(
+        dct: Mapping,
+        key: Any,
+        default: Union[T, None] = None
+    ) -> Union[Any, T]:
     """
     Returns value associated w/ key in the dictionary-
     -or the default if the key doesn't exist.
