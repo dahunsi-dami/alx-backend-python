b96f13d71e436c4abb196701d638732acd7063d7
dahunsi-dami <gbemigadare@gmail.com>
2024-10-22 17:44:23 +0100

    Write type annot func to return sum of float list.

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Type-annotated function sum_list."""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Returns the sum of a list of floats."""
+    return sum(input_list)
