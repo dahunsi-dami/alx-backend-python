72f55725daff7fb0754489d0f1e34d148f4dbb8f
dahunsi-dami <gbemigadare@gmail.com>
2024-10-22 17:21:47 +0100

    Write type annot func to concat 2 strings.

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Type-annotated function concat."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings and returns the concat str."""
+    return str1 + str2
