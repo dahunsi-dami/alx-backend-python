35d66a47542e9bc89634bf3d5448d1cf27d8c105
dahunsi-dami <gbemigadare@gmail.com>
2024-10-22 23:50:07 +0100

    Make type annot func returning func multiplying float by multiplier + other specs.

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Type-annotated function make_multiplier."""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Returns a func that multiplies a float by multiplier."""
+    def multiplier_function(x: float) -> float:
+        return x * multiplier
+
+    return multiplier_function
