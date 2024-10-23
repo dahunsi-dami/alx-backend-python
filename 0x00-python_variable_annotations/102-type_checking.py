f86e5c9a445aeb150c908dba858a5d5d58d1712c
dahunsi-dami <gbemigadare@gmail.com>
2024-10-23 01:12:08 +0100

    Revert to original code but turn array to tuple & return List, not tuple.

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
 """Type annotated zoom_array function."""
 
-from typing import Tuple, List, TypeVar
+from typing import Tuple, List, Any
 
-T = TypeVar('T')
-
-
-def zoom_array(lst: Tuple[T, ...], factor: int = 2) -> List[T]:
-    """Zooms in on element of input list by a given factor."""
-    zoomed_in: List[T] = [
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Zooms in on elements of input tuple by a given factor."""
+    zoomed_in: List = [
         item for item in lst
-        for _ in range(factor)
+        for i in range(factor)
     ]
     return zoomed_in
 
 
-array: Tuple[int, ...] = (12, 72, 91)
+array = (12, 72, 91)
 
-zoom_2x: List[int] = zoom_array(array)
+zoom_2x = zoom_array(array)
 
-zoom_3x: List[int] = zoom_array(array, 3)
+zoom_3x = zoom_array(array, 3)
