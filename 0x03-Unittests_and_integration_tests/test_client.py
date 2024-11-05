086602f8fbd6764f35953b13b37a5e80864db43a
dahunsi-dami <gbemigadare@gmail.com>
2024-11-06 00:11:55 +0100

    Make unittest to ensure _public_repos_url fetches repos_url properly.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Unittests for client.py module."""
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from client import GithubOrgClient
 from parameterized import parameterized
 
@@ -36,3 +36,24 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(expected_url)
 
         self.assertEqual(response, mock_get_json.return_value)
+
+    @patch.object(GithubOrgClient, 'org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        """
+        Test to ensure _public_repos_url fetches repos_url properly.
+
+        Args:
+            mock_org: the mocked org property.
+        """
+        mock_org.return_value = {
+            "repos_url": "https://api.github.com/orgs/testorg/repos"
+        }
+
+        client = GithubOrgClient("testorg")
+        result = client._public_repos_url
+
+        self.assertEqual(
+            result,
+            "https://api.github.com/orgs/testorg/repos"
+        )
+        mock_org.assert_called_once()
